bf58dfd8f3a4934269688e1d9b4eda6f41a5b122
Omar Jallow <omjallowwatf@outlook.com>
2023-11-22 11:41:54 +0000

    Added ShoppingCartItem model and defined it in AppDbContext.

--- DrinkAndGo2/Data/AppDbContext.cs
+++ DrinkAndGo2/Data/AppDbContext.cs
@@ -11,5 +11,6 @@ namespace DrinkAndGo2.Data
         }
         public DbSet<Drink> Drinks { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<ShoppingCartItem> ShoppingCartItems { get; set;}
     }
 }
